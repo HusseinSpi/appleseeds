5226ea83c2b006b7bcbd80d71a73defd2d457096
HusseinSpi <huseeink852@gmail.com>
2024-05-11 12:34:12 +0300

    as

--- RPG Gmae/Basic Objects/test.js
+++ RPG Gmae/Basic Objects/test.js
@@ -11,10 +11,46 @@
 //   return arr;
 // };
 
-console.log(fibonacci(12));
+// console.log(myFuncDecl()); // What will be the output?
+// function myFuncDecl() {
+//   return "Hello from function declaration!";
+// }
+// console.log(myFuncExpr()); // What will be the output?
+// var myFuncExpr = function () {
+//   return "Hello from function expression!";
+// };
 
-const fibonacci = (n) => {
-  if (n === 0) {
-    return 1;
+// const x = "global";
+// function outerFunc() {
+//   let y = "outer";
+//   function innerFunc() {
+//     var z = "inner";
+//     console.log(x); // What will be the output?
+//     console.log(y); // What will be the output?
+//   }
+//   innerFunc();
+//   console.log(z); // What will be the output?
+// }
+// outerFunc();
+
+const a = 1;
+function b() {
+  const a = 10;
+  c();
+  return;
+  function c() {
+    console.log(a); // What will be the output?
   }
-};
+}
+b();
+console.log(a); // What will be the output of a?
+foo();
+function foo() {
+  console.log(x); // What will be the output?
+  var x = "Hello, world!";
+  bar();
+  function bar() {
+    console.log(x); // What will be the output?
+    let x = "Hello, scope!";
+  }
+}
---
+++ RPG Gmae/class.js
@@ -0,0 +1,6 @@
+class Character {
+  constructor(name) {
+    this.name = name;
+    this._health = 100;
+  }
+}
