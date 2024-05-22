249fa3577a0027b007d5c864b7b49aab4791caf5
HusseinSpi <Huseeink852@gmail.com>
2024-05-22 12:55:38 +0300

    add four Exercise from Await

--- promises/Ex 6/script.js
+++ promises/Ex 6/script.js
@@ -1,16 +1,34 @@
-const 
+// class Movie {
+//   constructor(title, releaseDate, picture, rating) {
+//     this.title = title;
+//     this.releaseDate = releaseDate;
+//     this.picture = picture;
+//     this.rating = rating;
+//   }
+//   getTitle() {
+//     return this.title;
+//   }
+//   getReleaseDate() {
+//     return this.releaseDate;
+//   }
+//   getPicture() {
+//     return this.picture;
+//   }
+//   getRating() {
+//     return this.rating;
+//   }
+// }
 
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-  },
-};
+// const getHttpOptions = () => ({
+//   method: "GET",
+//   headers: {
+//     accept: "application/json",
+//     Authorization: "Bearer af01626b2f1fb8a5ad802cb6052bb7a0",
+//   },
+// });
 
-fetch(
-  "https://api.themoviedb.org/3/movie/550?api_key=",
-  options
-)
+// const API_BASE_URL = "https://api.themoviedb.org/3/";
+
+fetch(`https://api.themoviedb.org/3/movie/550?api_key=`)
   .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+  .then((data) => console.log(data));
