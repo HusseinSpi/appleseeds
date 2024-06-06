51bad0cb53a25801c51625eb9e1449367ef1e4d4
HusseinSpi <Huseeink852@gmail.com>
2024-06-06 15:37:22 +0300

    context

--- React/grocery-bud/src/App.jsx
+++ React/grocery-bud/src/App.jsx
@@ -1,23 +1,13 @@
-import { useState } from 'react';
-import Form from './Form';
-import { nanoid } from 'nanoid';
-import Items from './Items';
-import { ToastContainer, toast } from 'react-toastify';
-
-const getLocalStorage = () => {
-  let list = localStorage.getItem('list');
-  if (list) {
-    list = JSON.parse(localStorage.getItem('list'));
-  } else {
-    list = [];
-  }
-  return list;
-};
+import { useState } from "react";
+import Form from "./Form";
+import { nanoid } from "nanoid";
+import Items from "./Items";
+import { ToastContainer, toast } from "react-toastify";
 
 const setLocalStorage = (items) => {
-  localStorage.setItem('list', JSON.stringify(items));
+  localStorage.setItem("list", JSON.stringify(items));
 };
-const defaultList = JSON.parse(localStorage.getItem('list') || '[]');
+const defaultList = JSON.parse(localStorage.getItem("list") || "[]");
 const App = () => {
   const [items, setItems] = useState(defaultList);
 
@@ -30,14 +20,14 @@ const App = () => {
     const newItems = [...items, newItem];
     setItems(newItems);
     setLocalStorage(newItems);
-    toast.success('item added to the list');
+    toast.success("item added to the list");
   };
 
   const removeItem = (itemId) => {
     const newItems = items.filter((item) => item.id !== itemId);
     setItems(newItems);
     setLocalStorage(newItems);
-    toast.success('item deleted');
+    toast.success("item deleted");
   };
 
   const editItem = (itemId) => {
@@ -52,8 +42,8 @@ const App = () => {
     setLocalStorage(newItems);
   };
   return (
-    <section className='section-center'>
-      <ToastContainer position='top-center' />
+    <section className="section-center">
+      <ToastContainer position="top-center" />
       <Form addItem={addItem} />
       <Items items={items} removeItem={removeItem} editItem={editItem} />
     </section>
